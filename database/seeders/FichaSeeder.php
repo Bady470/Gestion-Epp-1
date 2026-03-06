955de7e34985c6c2b607de1eddcecd0830a63985
bady470 <mendozaalfonso713@gmail.com>
2026-03-06 11:21:47 -0500

    actulizar los seeder fichaseeder y programaseeder

--- database/seeders/FichaSeeder.php
+++ database/seeders/FichaSeeder.php
@@ -4,33 +4,254 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Ficha;
-use Illuminate\Support\Facades\DB;
 
 class FichaSeeder extends Seeder
 {
     public function run()
     {
-        $csvFile = base_path("database/csv/programas_fichas_areas.csv");
-        $data = array_map("str_getcsv", file($csvFile));
-        $header = array_shift($data); // Remove header row
-
-        foreach ($data as $row) {
-            if (count($row) < 4) continue; // Skip malformed rows
-
-            $rowData = array_combine($header, $row);
-
-            $programName = trim($rowData["NOMBRE_PROGRAMA_FORMACION"]);
-            $fichaNumero = trim($rowData["FICHA"]);
-
-            $programa = DB::table("programas")->where("nombre", $programName)->first();
-            $programaId = $programa ? $programa->id : null;
-
-            if ($programaId) {
-                Ficha::create([
-                    "numero" => $fichaNumero,
-                    "programas_id" => $programaId,
-                ]);
-            }
-        }
+        Ficha::create([
+            'numero' => '2998167',
+            'programas_id' => 1,
+        ]);
+        Ficha::create([
+            'numero' => '2997942',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '2998150',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '2930432',
+            'programas_id' => 3,
+        ]);
+        Ficha::create([
+            'numero' => '2930433',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3357679',
+            'programas_id' => 5,
+        ]);
+        Ficha::create([
+            'numero' => '2998177',
+            'programas_id' => 6,
+        ]);
+        Ficha::create([
+            'numero' => '2997972',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '2998174',
+            'programas_id' => 3,
+        ]);
+        Ficha::create([
+            'numero' => '2997931',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3308137',
+            'programas_id' => 7,
+        ]);
+        Ficha::create([
+            'numero' => '3308026',
+            'programas_id' => 8,
+        ]);
+        Ficha::create([
+            'numero' => '3308040',
+            'programas_id' => 8,
+        ]);
+        Ficha::create([
+            'numero' => '3066542',
+            'programas_id' => 9,
+        ]);
+        Ficha::create([
+            'numero' => '3066553',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3066560',
+            'programas_id' => 10,
+        ]);
+        Ficha::create([
+            'numero' => '3234481',
+            'programas_id' => 11,
+        ]);
+        Ficha::create([
+            'numero' => '3233363',
+            'programas_id' => 12,
+        ]);
+        Ficha::create([
+            'numero' => '3234475',
+            'programas_id' => 13,
+        ]);
+        Ficha::create([
+            'numero' => '3328394',
+            'programas_id' => 14,
+        ]);
+        Ficha::create([
+            'numero' => '3408170',
+            'programas_id' => 15,
+        ]);
+        Ficha::create([
+            'numero' => '3066580',
+            'programas_id' => 16,
+        ]);
+        Ficha::create([
+            'numero' => '3066539',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3066552',
+            'programas_id' => 6,
+        ]);
+        Ficha::create([
+            'numero' => '3317554',
+            'programas_id' => 17,
+        ]);
+        Ficha::create([
+            'numero' => '3391946',
+            'programas_id' => 18,
+        ]);
+        Ficha::create([
+            'numero' => '3394312',
+            'programas_id' => 19,
+        ]);
+        Ficha::create([
+            'numero' => '3174638',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3174668',
+            'programas_id' => 9,
+        ]);
+        Ficha::create([
+            'numero' => '3174634',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3174600',
+            'programas_id' => 20,
+        ]);
+        Ficha::create([
+            'numero' => '3174672',
+            'programas_id' => 1,
+        ]);
+        Ficha::create([
+            'numero' => '3174412',
+            'programas_id' => 21,
+        ]);
+        Ficha::create([
+            'numero' => '3174456',
+            'programas_id' => 22,
+        ]);
+        Ficha::create([
+            'numero' => '3174444',
+            'programas_id' => 3,
+        ]);
+        Ficha::create([
+            'numero' => '3174308',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3174404',
+            'programas_id' => 6,
+        ]);
+        Ficha::create([
+            'numero' => '3223907',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3290547',
+            'programas_id' => 23,
+        ]);
+        Ficha::create([
+            'numero' => '3312006',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3234511',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3234484',
+            'programas_id' => 24,
+        ]);
+        Ficha::create([
+            'numero' => '3296585',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3308127',
+            'programas_id' => 25,
+        ]);
+        Ficha::create([
+            'numero' => '3290577',
+            'programas_id' => 3,
+        ]);
+        Ficha::create([
+            'numero' => '3312002',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3290911',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3370847',
+            'programas_id' => 10,
+        ]);
+        Ficha::create([
+            'numero' => '3370845',
+            'programas_id' => 10,
+        ]);
+        Ficha::create([
+            'numero' => '3232951',
+            'programas_id' => 3,
+        ]);
+        Ficha::create([
+            'numero' => '3233006',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3232972',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3369736',
+            'programas_id' => 6,
+        ]);
+        Ficha::create([
+            'numero' => '3409184',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3409215',
+            'programas_id' => 1,
+        ]);
+        Ficha::create([
+            'numero' => '3409194',
+            'programas_id' => 9,
+        ]);
+        Ficha::create([
+            'numero' => '3409175',
+            'programas_id' => 2,
+        ]);
+        Ficha::create([
+            'numero' => '3409199',
+            'programas_id' => 6,
+        ]);
+        Ficha::create([
+            'numero' => '3409185',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3409186',
+            'programas_id' => 4,
+        ]);
+        Ficha::create([
+            'numero' => '3369746',
+            'programas_id' => 26,
+        ]);
     }
 }
--- database/seeders/ProgramaSeeder.php
+++ database/seeders/ProgramaSeeder.php
@@ -9,37 +9,139 @@ class ProgramaSeeder extends Seeder
 {
     public function run()
     {
-        $csvFile = base_path("database/csv/programas_fichas_areas.csv");
-        $data = array_map("str_getcsv", file($csvFile));
-        $header = array_shift($data); // Remove header row
+        $programas = [
+            [
+                'nombre' => 'PRODUCCION DE MULTIMEDIA',
+                'areas_id' => 7,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'GESTIÓN EMPRESARIAL',
+                'areas_id' => 4,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'GESTION CONTABLE Y DE INFORMACION FINANCIERA',
+                'areas_id' => 4,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'COORDINACION EN SISTEMAS INTEGRADOS DE GESTION',
+                'areas_id' => 4,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'BISUTERIA ARTESANAL.',
+                'areas_id' => 9,
+                'nivel' => 'OPERARIO',
+            ],
+            [
+                'nombre' => 'ANALISIS Y DESARROLLO DE SOFTWARE.',
+                'areas_id' => 7,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'OPERACION DE MAQUINARIA AGRICOLA',
+                'areas_id' => 2,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'EMPRENDIMIENTO Y FOMENTO EMPRESARIAL',
+                'areas_id' => 9,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'PRODUCCIÓN DE ESPECIES MENORES',
+                'areas_id' => 1,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'PRODUCCIÓN AGROPECUARIA ECOLÓGICA',
+                'areas_id' => 6,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'AGROTRONICA',
+                'areas_id' => 9,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'ELABORACION DE PRODUCTOS ALIMENTICIOS.',
+                'areas_id' => 9,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'INSTALACIONES HIDRAULICAS Y SANITARIAS EN EDIFICACIONES RESIDENCIALES Y COMERCIALES.',
+                'areas_id' => 3,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'SISTEMAS AGROPECUARIOS ECOLOGICOS.',
+                'areas_id' => 6,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'PROMOTOR DE SALUD',
+                'areas_id' => 9,
+                'nivel' => 'AUXILIAR',
+            ],
+            [
+                'nombre' => 'CONTROL DE CALIDAD EN LA INDUSTRIA DE ALIMENTOS',
+                'areas_id' => 5,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'COCINA.',
+                'areas_id' => 9,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'SANEAMIENTO Y SALUD AMBIENTAL',
+                'areas_id' => 9,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'ELABORACION DE PRENDAS DE VESTIR SOBRE MEDIDAS',
+                'areas_id' => 8,
+                'nivel' => 'TÉCNICO',
+            ],
+            [
+                'nombre' => 'COMUNICACIÓN COMERCIAL',
+                'areas_id' => 4,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'GESTION DE LA PRODUCCION AGRICOLA',
+                'areas_id' => 2,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'CONTROL DE CALIDAD EN LA INDUSTRIA DE ALIMENTOS',
+                'areas_id' => 9,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'PRODUCCIÓN GANADERA',
+                'areas_id' => 1,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'GESTIÓN DE RECURSOS NATURALES',
+                'areas_id' => 6,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'MECANIZACIÓN AGRÍCOLA',
+                'areas_id' => 2,
+                'nivel' => 'TECNÓLOGO',
+            ],
+            [
+                'nombre' => 'GESTION DE PROYECTOS DE DESARROLLO ECONOMICO Y SOCIAL',
+                'areas_id' => 9,
+                'nivel' => 'TECNÓLOGO',
+            ],
+        ];
 
-        $processedPrograms = [];
-
-        foreach ($data as $row) {
-            if (count($row) < 4) continue; // Skip malformed rows
-
-            $rowData = array_combine($header, $row);
-
-            $areaName = trim($rowData["AREA"]);
-            $programName = trim($rowData["NOMBRE_PROGRAMA_FORMACION"]);
-            $nivel = trim($rowData["NIVEL"]);
-
-            // Default to "SIN ÁREA DEFINIDA" if area not found or empty
-            if (empty($areaName)) {
-                $areaName = "SIN ÁREA DEFINIDA";
-            }
-
-            $area = DB::table("areas")->where("nombre", $areaName)->first();
-            $areaId = $area ? $area->id : null;
-
-            if ($areaId && !isset($processedPrograms[$programName])) {
-                DB::table("programas")->insertOrIgnore([
-                    "nombre" => $programName,
-                    "areas_id" => $areaId,
-                    "nivel" => $nivel,
-                ]);
-                $processedPrograms[$programName] = true;
-            }
-        }
+        DB::table('programas')->insert($programas);
     }
 }
